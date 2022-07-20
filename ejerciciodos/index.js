3700df25d8407fdd4d3e042ef2a7632fd7a878d4
Hermann Heyer <h.heyer@docentes.cftmaule.cl>
2022-07-20 14:16:01 -0400

    Tarea dos

---
+++ ejerciciodos/index.js
@@ -0,0 +1,9 @@
+const libro = {
+    titulo : "Clean Code",
+    autor  : "Robert C. Martin",
+    fecha  : Date("2009,01,20"),
+    url    : null
+}
+const lista = ["Hermann Heyer",32,true,"01-20-1990",libro]
+
+console.log(lista)
