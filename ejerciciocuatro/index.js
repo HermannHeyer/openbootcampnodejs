3c48c3e943d67c097e1e422bec126a00ffccccc2
Hermann Heyer <h.heyer@docentes.cftmaule.cl>
2022-07-20 20:22:36 -0400

    Tarea Numero 4

---
+++ ejerciciocuatro/index.js
@@ -0,0 +1,13 @@
+let nombre = "Hermann"
+let apellido = "Heyer"
+let estudiante = ` Estudiante : ${nombre} ${apellido}`
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let numero_letras = estudiante.length
+let primera_letra = nombre.charAt(0);
+let ultima_letra = apellido.substr(-1)
+let sin_espacios = estudiante.replace(/ /g, "")
+let existe = nombre.includes("pedro")
+
+
+
