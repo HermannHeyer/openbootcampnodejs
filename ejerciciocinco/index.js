9137cf4f71c6ed1e3b124f9b763f1ef3e6ff86e0
Hermann Heyer <h.heyer@docentes.cftmaule.cl>
2022-07-20 23:40:28 -0400

    Tarea Numero cinco

---
+++ ejerciciocinco/index.js
@@ -0,0 +1,9 @@
+let centimetros = 170;
+let altura = 1.70;
+let peso = 80.2
+let altura_arriba = Math.round(altura)
+let peso_abajo = Math.round(peso)
+let max = Number.MAX_VALUE
+console.log(max)
+let maxuno = Number.MAX_VALUE + 1
+console.log(maxuno)
